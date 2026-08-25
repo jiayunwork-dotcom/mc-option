ad1019f3ededff31cdc8782effd00a0bc317f214
“jiayun” <jiayun_work@163.com>
2026-08-25 23:47:19 +0800

    fix: initialize knockBinder map before BindKnockLive write

--- internal/engine/knock_bind.go
+++ internal/engine/knock_bind.go
@@ -8,6 +8,7 @@ var liveKnock knockBinder
 
 func BindKnockLive(value float64) {
 	if liveKnock.byValue == nil {
+		liveKnock.byValue = make(map[float64]float64)
 	}
 	liveKnock.byValue[value] = value
 }
